2d66702d90ea9f1292839e478f186bb281c251f1
xiaohehe <1296522554@qq.com>
2022-03-17 02:34:49 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="app-container">
-    App.vue
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login-container">
+    Login
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "Login"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
