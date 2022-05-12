07bd0693b1b7f8a069a274279af1d877effc5193
xiaohehe <1296522554@qq.com>
2022-05-12 14:21:17 +0800

    完成登录功能 + 引入Element Plus 组件库

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -6,15 +6,15 @@
           <h1>注册</h1>
           <input type="text" placeholder="取一个账号吧~" v-model="register.username"/>
           <input type="password" placeholder="输入您的密码" v-model="register.password"/>
-          <button @click="signUp">注册</button>
+          <button @click="onSignUp">注册</button>
         </div>
       </div>
       <div class="form-container sign-in-container">
         <div class="container-box">
           <h1>登录</h1>
-          <input type="text" placeholder="输入登录账号"/>
-          <input type="password" placeholder="输入登录密码"/>
-          <button>登录</button>
+          <input type="text" placeholder="输入登录账号" v-model="login.username" @keyup.enter="onLogin"/>
+          <input type="password" placeholder="输入登录密码" v-model="login.password" @keyup.enter="onLogin"/>
+          <button @click="onLogin">登录</button>
         </div>
       </div>
       <div class="overlay-container">
@@ -39,6 +39,7 @@
 import {ref, reactive} from 'vue';
 import Auth from '../apis/auth';
 import {useRouter} from 'vue-router';
+import {ElMessage} from 'element-plus';
 
 export default {
   name: "Login",
@@ -51,8 +52,7 @@ export default {
       username: '',
       password: ''
     })
-
-    const signUp = () => {
+    const onSignUp = () => {
       if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(register.username)) {
         return alert('用户名3~15个字符，仅限于字母数字下划线中文')
       }
@@ -63,16 +63,40 @@ export default {
       Auth.register({
         username: register.username,
         password: register.password
-      }).then(() => {
+      }).then(res => {
+        ElMessage.success(res.msg)
         router.replace('/notebooks')
       })
 
     }
 
+    const login = reactive({
+      username: 'admin66',
+      password: '12345678'
+    })
+    const onLogin = () => {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(login.username)) {
+        return alert('用户名3~15个字符，仅限于字母数字下划线中文')
+      }
+      if (!/^.{6,16}$/.test(login.password)) {
+        return alert('密码长度为6~16个字符')
+      }
+
+      Auth.login({
+        username: login.username,
+        password: login.password
+      }).then((res) => {
+        ElMessage.success(res.msg)
+        router.replace('/notebooks')
+      })
+    }
+
     return {
       isRegisteredPage,
       register,
-      signUp
+      onSignUp,
+      login,
+      onLogin
     }
   }
 }
