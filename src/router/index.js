fff1c7a7fb69b8e377ae2531b64354409cb0a063
xiaohehe <1296522554@qq.com>
2022-05-12 12:15:33 +0800

    抽离 路由配置对象

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
-import { createRouter,createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import Login from "../views/Login.vue";
 
+
+// 路由配置对象
+const routes = [
+    {path: '/', component: Login}
+]
+
 const router = createRouter({
-    history:createWebHistory(),
-    routes:[
-        {
-            path:'/',
-            component: Login,
-        }
-    ]
+    history: createWebHistory(),
+    routes
 })
 export default router;
