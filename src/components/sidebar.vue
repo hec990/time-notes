82d3d5856c48c2c7c079b43e3f833fc1155aa8ac
xiaohehe <1296522554@qq.com>
2022-05-21 09:57:57 +0800

    完成侧边栏导航样式

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
     <sidebarAvatar></sidebarAvatar>
+    <sidebarNav></sidebarNav>
   </div>
 </template>
 
 <script>
 import sidebarAvatar from "./sidebarAvatar.vue";
+import sidebarNav from "./sidebarNav.vue";
 export default {
   name: "sidebar",
   components:{
-    sidebarAvatar
+    sidebarAvatar,
+    sidebarNav
   }
 }
 </script>
---
+++ src/components/sidebarNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="line">
+      <p></p>
+    </div>
+    <nav class="navList">
+      <div>笔记本详情</div>
+      <div>笔记本列表</div>
+      <div>回收站</div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sidebarNav"
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  .line {
+    padding: 10px 15px 0 15px;
+
+    p {
+      border-top: 1px solid #E5E5E5;
+    }
+  }
+
+  .navList {
+    display: flex;
+    flex-direction: column;
+    margin-top: 15px;
+
+    div {
+      text-align: center;
+      padding: 10px;
+    }
+
+    div:hover {
+      background: rgba(0, 0, 0, .06);
+    }
+  }
+}
+</style>
