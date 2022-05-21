daca23c7e93c7df16491a463fab11d374c68af49
xiaohehe <1296522554@qq.com>
2022-05-21 14:48:47 +0800

    修改路由配置：将一级路由修改为home下的嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,21 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "../pages/Login.vue";
 import NoteBookList from "../pages/NoteBookList.vue";
-import Home from "../pages/Home";
-import NoteDetail from "../pages/NoteDetail";
-import TrashDetail from "../pages/TrashDetail";
+import Home from "../pages/Home.vue";
+import NoteDetail from "../pages/NoteDetail.vue";
+import TrashDetail from "../pages/TrashDetail.vue";
 
 
 // 路由配置对象
 const routes = [
     {path: '/', component: Login},
-    {path: '/home',component: Home},
-    {path: '/notebooks', component: NoteBookList},
-    {path: '/note', component: NoteDetail},
-    {path: '/trash', component: TrashDetail},
+    {
+        path: '/home', component: Home, children: [
+            {path: 'notebooks', component: NoteBookList},
+            {path: 'note', component: NoteDetail},
+            {path: 'trash', component: TrashDetail},
+        ]
+    },
 ]
 
 const router = createRouter({
