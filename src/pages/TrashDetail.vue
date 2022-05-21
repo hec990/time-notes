f1e67d36ffd94b92c57b7e913a57129d94b3fc8e
xiaohehe <1296522554@qq.com>
2022-05-21 14:36:56 +0800

    创建三个组件页面，并且配置了路由

---
+++ src/pages/TrashDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  TrashDetail
+</template>
+
+<script>
+export default {
+  name: "TrashDetail"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "../pages/Login.vue";
 import NoteBookList from "../pages/NoteBookList.vue";
+import Home from "../pages/Home";
+import NoteDetail from "../pages/NoteDetail";
+import TrashDetail from "../pages/TrashDetail";
 
 
 // 路由配置对象
 const routes = [
     {path: '/', component: Login},
-    {path: '/notebooks', component: NoteBookList}
+    {path: '/home',component: Home},
+    {path: '/notebooks', component: NoteBookList},
+    {path: '/note', component: NoteDetail},
+    {path: '/trash', component: TrashDetail},
 ]
 
 const router = createRouter({
