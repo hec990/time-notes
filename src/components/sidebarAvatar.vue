1738b0f15ad04822fd82e2a0c0b557d3c9174172
xiaohehe <1296522554@qq.com>
2022-05-22 11:55:18 +0800

    展示用户名信息

--- src/components/sidebarAvatar.vue
+++ src/components/sidebarAvatar.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container">
     <div class="userInfo">
-      <div class="logo">H</div>
-      <div class="name">{{name + '的空间'}}</div>
+      <div class="logo">
+        <span>{{ name.charAt(0) }}</span>
+      </div>
+      <div class="name">{{ name + '的空间' }}</div>
     </div>
   </div>
 </template>
 
 <script>
 import {ref} from 'vue';
+import Auth from '../apis/auth'
+
 export default {
   name: "sidebarAvatar",
-  setup(){
-    const name = ref('coderwhy');
+  setup() {
+    const name = ref('');
+
+    Auth.getInfo().then(res => {
+      console.log(res)
+      name.value = res.data.username;
+    })
 
     return {
       name
@@ -26,31 +35,35 @@ export default {
   min-height: 50px;
   padding-top: 10px;
   cursor: pointer;
+
   .userInfo {
     display: flex;
     align-items: center;
-    padding: 5px 0;
+    padding: 5px 0 5px 10px;
     border-radius: .25rem;
+
+    &:hover {
+      background: rgba(0, 0, 0, .06);
+    }
 
     .logo {
       width: 48px;
       height: 48px;
       border: 2px solid #ccc;
-      margin-left: 10px;
       border-radius: 50%;
-      text-align: center;
-      font-size: 28px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
       color: orange;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      span {
+        font-size: 28px;
+      }
     }
+
     .name {
       padding: 0 15px;
     }
-  }
-  .userInfo:hover {
-    background: rgba(0,0,0,.06);
   }
 }
 </style>
